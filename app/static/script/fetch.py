2d80f92ece0cd1b5f364e3b5822cbf7015f6281c
ymz1071 <ymz1071@175game.com>
2020-09-25 17:32:23 +0800

    use shareURL to parser

--- app/static/script/fetch.py
+++ app/static/script/fetch.py
@@ -2,6 +2,7 @@ import base64
 import os
 import random
 import re
+import time
 from concurrent.futures.thread import ThreadPoolExecutor
 
 from bs4 import BeautifulSoup, Comment
@@ -18,8 +19,8 @@ headers = {'Referer': 'http://www.91porn.com/index.php', 'Domain-Name': 'porn9_v
            }
 headers_1 = {
     'Referer': 'http://www.91porn.com/index.php', 'Domain-Name': 'porn9_video_domain_name',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    # 'X-Forwarded-For': '104.206.141.12'
+    # 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'X-Forwarded-For': '104.216.141.12'
 }
 url_aria2 = 'http://104.206.144.11:6800/jsonrpc'
 
@@ -45,13 +46,28 @@ def get_mf_list(url):
 
 
 def fetch_html(url, header):
-    resp = s.get(url=url, headers=header)
-    if (resp.status_code != 200 or (resp.text.find('你每天只可观看25个视频') != -1) or resp.text == None):
-        # headers_1['X-Forwarded-For'] = str(random.randrange(0, 255)) + '.' + str(
-        #     random.randrange(0, 255)) + '.' + str(random.randrange(0, 255)) + '.' + str(random.randrange(0, 255))
-        # return fetch_html(url, headers_1)
-        print(f"statusCode={resp.status_code}")
-        return None
+    headers_1['X-Forwarded-For'] = str(random.randrange(0, 255)) + '.' + str(
+        random.randrange(0, 255)) + '.' + str(random.randrange(0, 255)) + '.' + str(random.randrange(0, 255))
+    resp = s.get(url=url, headers=headers_1)
+    if resp is None or resp.status_code != 200 or resp.text.find('你每天只可观看25个视频') != -1:
+        time.sleep(0.5)
+        return fetch_html(url, headers_1)
+    else:
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        area = soup.find('textarea')
+        if area is not None:
+            video = area.text
+        else:
+            print(f"non share URL={url}")
+            return None
+        return fetch_share(video)
+
+
+def fetch_share(url):
+    resp = s.get(url, headers=headers)
+    if resp.status_code != 200 or resp is None:
+        time.sleep(0.5)
+        return fetch_share(url)
     else:
         return resp.text
 
@@ -65,9 +81,9 @@ def parse_url(title, url):
             param2 = match.group(1)
         else:
             print(f"not encryption:{url}")
-            soup=BeautifulSoup(html,'html.parser')
-            video=soup.find('video').find('source').get('src')
-            upload(video,title)
+            soup = BeautifulSoup(html, 'html.parser')
+            video = soup.find('video').find('source').get('src')
+            upload(video, title)
             return
         param1 = str(base64.decodebytes(str.encode(param1)), 'utf-8')
         str_org = ''
@@ -86,8 +102,8 @@ def upload(url, name):
     print("url=" + url)
     file_path = ''
     try:
-        r = s.get(url=url, headers=headers_1,allow_redirects=True, stream=True)
-        if r.status_code!=200:
+        r = s.get(url=url, headers=headers_1, allow_redirects=True, stream=True)
+        if r.status_code != 200:
             print(r)
             return
         file_path = os.path.join(STATIC_DIR, f"{name}.mp4")
@@ -114,8 +130,9 @@ def get_all(category, s_page: int, e_page: int):
 
 
 if __name__ == '__main__':
-    get_all('tf', 1, 2)
-    #parse_url("tete","http://www.91porn.com/view_video.php?viewkey=976079af64581f7b1430&page=1&viewtype=basic&category=tf")
+    get_all('tf', 5, 7)
+    # parse_url("jj",'http://91.9p9.xyz/ev.php?VID=bf54JLJFfztRZFFXLdE8zkkEbCqhPrm3sDulKFKSPFyKmc42')
+    # parse_url("tete","http://www.91porn.com/view_video.php?viewkey=248dc390fd47f747d762&page=5&viewtype=basic&category=tf")
     # mf_url = 'http://www.91porn.com/v.php?category=mf&viewtype=basic&page={0}'.format(1)
     # get_mf_list(mf_url)
     # _thread.start_new_thread(get_mf_list, (mf_url,))
